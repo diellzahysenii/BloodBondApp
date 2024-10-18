cc08165fd2de69da0594ffac8618b9a32dadac28
Diellza <diellzahyseni2003@gmail.com>
2024-10-18 22:07:56 +0200

    Initial Commit

---
+++ app/src/main/java/com/example/bloodbondapp/Activities/SplashScreen.java
@@ -0,0 +1,42 @@
+package com.example.bloodbondapp.Activities;
+
+import static com.example.bloodbondapp.R.*;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.bloodbondapp.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash_screen);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+
+        }); // Closing lambda expression properly
+
+        // Handler to add delay for the splash screen
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                finish();
+            }
+        }, 2500); // delayMillis should be passed inside the method call
+    }
+}
