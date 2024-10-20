93255ca09cd5f48963034cd6b9d3b9858a930ebf
Diellza <diellzahyseni2003@gmail.com>
2024-10-21 00:21:55 +0200

    Login Activity view

--- app/src/main/java/com/example/bloodbondapp/Activities/RegisterActivity.java
+++ app/src/main/java/com/example/bloodbondapp/Activities/RegisterActivity.java
@@ -1,6 +1,10 @@
 package com.example.bloodbondapp.Activities;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +15,8 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.bloodbondapp.R;
 
 public class RegisterActivity extends AppCompatActivity {
+    private EditText nameEt, cityEt, bloodGroupEt, passwordEt, mobileEt;
+    private Button submitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +27,28 @@ public class RegisterActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        nameEt = findViewById(R.id.name);
+        cityEt = findViewById(R.id.city);
+        bloodGroupEt = findViewById(R.id.bloodtype);
+        passwordEt = findViewById(R.id.password);
+        mobileEt = findViewById(R.id.number);
+        submitButton = findViewById(R.id.submit_button);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name, city, blood_group, password, mobile;
+                name = nameEt.getText().toString();
+                city = cityEt.getText().toString();
+                blood_group = bloodGroupEt.getText().toString();
+                password = passwordEt.getText().toString();
+                mobile = mobileEt.getText().toString();
+                if (name.length()==0 || city.length()==0 || blood_group.length()==0 || password.length()==0 || mobile.length()==0){
+                    Toast.makeText(RegisterActivity.this, "Please fill all the details", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(RegisterActivity.this, "Successfully Registered", Toast.LENGTH_SHORT).show();
+                }
+            }
         });
     }
 }
